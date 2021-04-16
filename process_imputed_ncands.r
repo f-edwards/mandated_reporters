7ed7df0e20c2a1f04bad85fcd783a9c8a22b9631
f-edwards <fedwards@uw.edu>
2021-04-16 09:22:59 -0700

    fast forward

--- process_imputed_ncands.r
+++ process_imputed_ncands.r
@@ -2,7 +2,7 @@
 ## read in imputed ncands from cps_lifetables imputation run
 ## for mandated reporters analysis
 ### output full file for main read.r
-
+rm(list=ls())
 gc()
 library(tidyverse)
 library(mice)
@@ -10,36 +10,28 @@ library(mice)
 
 #### PAUSE TO MAKE IT SO I DONT NEED TO JOIN!!!!
 
-# ncands1<-readRDS("./data/ncands_imps1.RDS") %>% 
-#   mice::complete(action = "long", include = F) %>% 
-#   select(-pct_aian, -pct_api, -pct_Black, -pct_Latinx,
-#          -sex)
-# ncands2<-readRDS("./data/ncands_imps2.RDS")%>% 
-#   mice::complete(action = "long", include = F) %>% 
-#   select(-pct_aian, -pct_api, -pct_Black, -pct_Latinx,
-#          -sex)
-# ncands3<-readRDS("./data/ncands_imps3.RDS")%>% 
-#   mice::complete(action = "long", include = F) %>% 
-#   select(-pct_aian, -pct_api, -pct_Black, -pct_Latinx,
-#          -sex)
-# ncands4<-readRDS("./data/ncands_imps4.RDS")%>% 
-#   mice::complete(action = "long", include = F) %>% 
-#   select(-pct_aian, -pct_api, -pct_Black, -pct_Latinx,
-#          -sex)
-ncands5<-readRDS("./data/ncands_imps5.RDS")%>% 
-  mice::complete(action = "long", include = F) %>% 
-  select(-pct_aian, -pct_api, -pct_Black, -pct_Latinx,
-         -sex)
+ncands1<-readRDS("./data/ncands_imps_subst1.RDS") %>%
+  mice::complete(action = "long", include = F)
+
+ncands2<-readRDS("./data/ncands_imps_subst2.RDS")%>%
+  mice::complete(action = "long", include = F)
+
+ncands3<-readRDS("./data/ncands_imps_subst3.RDS")%>%
+  mice::complete(action = "long", include = F)
+
+ncands4<-readRDS("./data/ncands_imps_subst4.RDS")%>%
+  mice::complete(action = "long", include = F)
+
+ncands5<-readRDS("./data/ncands_imps_subst5.RDS")%>% 
+  mice::complete(action = "long", include = F) 
+
+
+
 
 nat_reports<-function(x){
   temp<-x %>% 
-    mutate(race_ethn = as.character(race_ethn)) %>% 
-    mutate(race_ethn =
-             ifelse(race_ethn=="AI/AN", "AIAN", race_ethn),
-           race_ethn = ifelse(race_ethn=="Hispanic", "Latinx", race_ethn)) %>%
-    mutate(rptsrc = ifelse(is.na(rptsrc), "unknown", rptsrc)) %>% 
-    filter(!(is.na(race_ethn)), !is.na(age)) %>%  # check the 700+ missing post imputation
-    group_by(.imp, year, race_ethn, age, rptsrc, chmal1) %>% 
+    group_by(.imp, subyr, race_ethn, chage, rptsrc, chmal1,
+             rptvictim, drugs) %>% 
     summarise(n_total = n()) %>% 
     ungroup()
   
@@ -48,34 +40,35 @@ nat_reports<-function(x){
 
 state_reports<-function(x){
   temp<-x %>% 
-    mutate(race_ethn = as.character(race_ethn)) %>% 
-    mutate(race_ethn =
-             ifelse(race_ethn=="AI/AN", 
-                    "AIAN", 
-                    race_ethn),
-    race_ethn = ifelse(race_ethn=="Hispanic", "Latinx", race_ethn)) %>%
-    mutate(rptsrc = ifelse(is.na(rptsrc), "unknown", rptsrc)) %>% 
-    filter(!(is.na(race_ethn)), !is.na(age)) %>%  # check the 700+ missing post imputation
-    group_by(.imp, staterr, year, race_ethn, age, rptsrc, chmal1) %>% 
-    summarise(n_total = n())
-  
-  ### later add nreports, but can't be subset by kid, must be by rptsrc, can 
-  ### have variation across kids within reports
+    group_by(.imp, staterr, subyr, race_ethn, chage, rptsrc, chmal1,
+             rptvictim, drugs) %>% 
+    summarise(n_total = n()) %>% 
+    ungroup()
   
   return(temp)
 }
 
-# nat_1<-nat_reports(ncands1)
-# nat_2<-nat_reports(ncands2)
-# nat_3<-nat_reports(ncands3)
-# nat_4<-nat_reports(ncands4)
+nat_1<-nat_reports(ncands1)
+nat_2<-nat_reports(ncands2)
+nat_3<-nat_reports(ncands3)
+nat_4<-nat_reports(ncands4)
 nat_5<-nat_reports(ncands5)
 
-# nat_ncands<-bind_rows(
-#   nat_1, nat_2, nat_3, nat_4, nat_5)
+nat_ncands<-bind_rows(
+  nat_1, nat_2, nat_3, nat_4, nat_5)
 
 
+state_1<-state_reports(ncands1)
+state_2<-state_reports(ncands2)
+state_3<-state_reports(ncands3)
+state_4<-state_reports(ncands4)
 state_5<-state_reports(ncands5)
 
-nat_ncands<-nat_5
-state_ncands<-state_5
+state_ncands<-bind_rows(state_1,
+                        state_2, 
+                        state_3,
+                        state_4,
+                        state_5)
+
+saveRDS(nat_ncands, file = "nat_ncands.RDS")
+saveRDS(state_ncands, file = "state_ncands.RDS")
--- read.r
+++ read.r
@@ -1,3 +1,26 @@
+births<-read_tsv("./data/natality18.txt",
+                 guess_max = (1e5)) %>% 
+  slice(1:115) %>% 
+  filter(is.na(Notes)) %>% 
+  select(-Notes)
+
+births_grp<-births %>% 
+  mutate(race_ethn = 
+           case_when(
+             str_detect(`Mother's Single/Multi Race 31`,
+                        "AIAN") ~ "AIAN",
+             str_detect(`Mother's Single/Multi Race 31`,
+                        "Black") ~ "Black",
+             str_detect(`Mother's Single/Multi Race 31`,
+                        "Asian") ~ "Asian/PI",
+             str_detect(`Mother's Single/Multi Race 31`,
+                        "NHOPI") ~ "Asian/PI",
+             `Mother's Hispanic Origin` == "Hispanic or Latino" ~ "Latinx",
+             `Mother's Single/Multi Race 31`=="White (only)" ~ "White"
+           )) %>% 
+  group_by(race_ethn) %>% 
+  summarise(births = sum(Births))
+
 
 pop<-read_fwf("~/Projects/cps_lifetables/data/us.1990_2018.singleages.adjusted.txt",
               fwf_widths(c(4, 2, 2, 3, 2, 1, 1, 1, 2, 8),
@@ -21,3 +44,14 @@ pop<-pop%>%
   summarise(pop = sum(pop)) %>% 
   ungroup()
 
+
+#compare births to age 0 pop
+pop %>% 
+  filter(year==2018, age==0) %>% 
+  group_by(race_ethn) %>% 
+  summarise(pop = sum(pop))
+### looks fine
+
+saveRDS(births_grp, file = "births.RDS")
+
+saveRDS(pop, file = "pop.RDS")
