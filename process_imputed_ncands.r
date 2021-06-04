ec6d72cbe660faac06c547cbb4287ea82e2b9b7c
f-edwards <fedwards@uw.edu>
2021-06-04 03:15:27 -0700

    ff

--- process_imputed_ncands.r
+++ process_imputed_ncands.r
@@ -6,69 +6,70 @@ rm(list=ls())
 gc()
 library(tidyverse)
 library(mice)
+library(stringr)
 
 
-#### PAUSE TO MAKE IT SO I DONT NEED TO JOIN!!!!
-
-ncands1<-readRDS("./data/ncands_imps_subst1.RDS") %>%
-  mice::complete(action = "long", include = F)
-
-ncands2<-readRDS("./data/ncands_imps_subst2.RDS")%>%
-  mice::complete(action = "long", include = F)
-
-ncands3<-readRDS("./data/ncands_imps_subst3.RDS")%>%
-  mice::complete(action = "long", include = F)
-
-ncands4<-readRDS("./data/ncands_imps_subst4.RDS")%>%
-  mice::complete(action = "long", include = F)
-
-ncands5<-readRDS("./data/ncands_imps_subst5.RDS")%>% 
-  mice::complete(action = "long", include = F) 
-
-
-
+##### transform functions
 
 nat_reports<-function(x){
   temp<-x %>% 
+    mutate(chage = ifelse(chage==77, 0, chage)) %>% 
+    mutate(chage = ifelse(chage>100, chage-100, chage)) %>% ### two sets, 113, 114 will assume they are typos
     group_by(.imp, subyr, race_ethn, chage, rptsrc, chmal1,
-             rptvictim, drugs) %>% 
+             rptvictim, drugs, alc) %>% 
     summarise(n_total = n()) %>% 
-    ungroup()
+    ungroup()%>% 
+    mutate(rptsrc = str_to_title(rptsrc))
   
   return(temp)
 }
 
 state_reports<-function(x){
   temp<-x %>% 
+    mutate(chage = ifelse(chage==77, 0, chage)) %>% 
+    mutate(chage = ifelse(chage>100, chage-100, chage)) %>% ### two sets, 113, 114 will assume they are typos
     group_by(.imp, staterr, subyr, race_ethn, chage, rptsrc, chmal1,
-             rptvictim, drugs) %>% 
+             rptvictim, drugs, alc) %>% 
     summarise(n_total = n()) %>% 
-    ungroup()
+    ungroup()%>% 
+    mutate(rptsrc = str_to_title(rptsrc))
   
   return(temp)
 }
 
-nat_1<-nat_reports(ncands1)
-nat_2<-nat_reports(ncands2)
-nat_3<-nat_reports(ncands3)
-nat_4<-nat_reports(ncands4)
-nat_5<-nat_reports(ncands5)
 
-nat_ncands<-bind_rows(
-  nat_1, nat_2, nat_3, nat_4, nat_5)
+#### run the whole thing through a loop to speed up
+path_files<-paste("./data/", list.files("./data"), sep = "")
+filenames<-path_files[grep("subst", path_files)]
+
+#### drug/alc variables got messed up by 
+#### bug in pre-processing code
+#### will use imputed vars, attach observed for subst ab vars
 
 
-state_1<-state_reports(ncands1)
-state_2<-state_reports(ncands2)
-state_3<-state_reports(ncands3)
-state_4<-state_reports(ncands4)
-state_5<-state_reports(ncands5)
+#for(i in 1:length(filenames)){
+for(i in 1:length(filenames)){
 
-state_ncands<-bind_rows(state_1,
-                        state_2, 
-                        state_3,
-                        state_4,
-                        state_5)
+  temp<-readRDS(filenames[i])%>% 
+    mice::complete(action = "long", include = F)
+  
+  nat_temp<-nat_reports(temp)
+  state_temp<-state_reports(temp)
+  
+  if(i==1){
+    nat<-nat_temp
+    state<-state_temp
+  } else{
+    nat<-nat %>% 
+      bind_rows(nat_temp)
+    state<-state %>% 
+      bind_rows(state_temp)
+  }
+  
+  rm(temp)
+  gc()
+}
 
-saveRDS(nat_ncands, file = "nat_ncands.RDS")
-saveRDS(state_ncands, file = "state_ncands.RDS")
+
+saveRDS(nat, file = "nat_ncands.RDS")
+saveRDS(state, file = "state_ncands.RDS")
--- read.r
+++ read.r
@@ -1,38 +1,45 @@
-births<-read_tsv("./data/natality18.txt",
-                 guess_max = (1e5)) %>% 
-  slice(1:115) %>% 
-  filter(is.na(Notes)) %>% 
-  select(-Notes)
+library(tidyverse)
 
-births_grp<-births %>% 
-  mutate(race_ethn = 
-           case_when(
-             str_detect(`Mother's Single/Multi Race 31`,
-                        "AIAN") ~ "AIAN",
-             str_detect(`Mother's Single/Multi Race 31`,
-                        "Black") ~ "Black",
-             str_detect(`Mother's Single/Multi Race 31`,
-                        "Asian") ~ "Asian/PI",
-             str_detect(`Mother's Single/Multi Race 31`,
-                        "NHOPI") ~ "Asian/PI",
-             `Mother's Hispanic Origin` == "Hispanic or Latino" ~ "Latinx",
-             `Mother's Single/Multi Race 31`=="White (only)" ~ "White"
-           )) %>% 
-  group_by(race_ethn) %>% 
-  summarise(births = sum(Births))
+#### update the births data for 10-19
+### pull from CDC Wonder
+### how to think about matching
+### mother / father race data to child race data.... excluding for now
+### match won't be straightforward, just use age 0 SEER data
+# births<-read_tsv("./data/natality18.txt",
+#                  guess_max = (1e5)) %>% 
+#   slice(1:115) %>% 
+#   filter(is.na(Notes)) %>% 
+#   select(-Notes)
+# 
+# births_grp<-births %>% 
+#   mutate(race_ethn = 
+#            case_when(
+#              str_detect(`Mother's Single/Multi Race 31`,
+#                         "AIAN") ~ "AIAN",
+#              str_detect(`Mother's Single/Multi Race 31`,
+#                         "Black") ~ "Black",
+#              str_detect(`Mother's Single/Multi Race 31`,
+#                         "Asian") ~ "Asian/PI",
+#              str_detect(`Mother's Single/Multi Race 31`,
+#                         "NHOPI") ~ "Asian/PI",
+#              `Mother's Hispanic Origin` == "Hispanic or Latino" ~ "Latinx",
+#              `Mother's Single/Multi Race 31`=="White (only)" ~ "White"
+#            )) %>% 
+#   group_by(race_ethn) %>% 
+#   summarise(births = sum(Births))
 
 
-pop<-read_fwf("~/Projects/cps_lifetables/data/us.1990_2018.singleages.adjusted.txt",
+pop<-read_fwf("./data/us.1990_2019.singleages.adjusted.txt",
               fwf_widths(c(4, 2, 2, 3, 2, 1, 1, 1, 2, 8),
                          c("year", "state", "st_fips",
                            "cnty_fips", "reg", "race",
                            "hisp", "sex", "age", "pop"))) %>% 
-  filter(year>2013) 
+  filter(year>2009) 
 
 pop<-pop%>%
   mutate(pop = as.integer(pop),
          age = as.integer(age))%>%
-  filter(age<=18) %>% 
+  filter(age==0) %>% 
   mutate(race_ethn =
            case_when(
              race==1 & hisp ==0 ~ "White",
@@ -40,18 +47,18 @@ pop<-pop%>%
              race==3 ~ "AIAN", 
              race==4 ~ "Asian/PI",
              hisp==1 ~ "Latinx")) %>% 
-  group_by(year, state, st_fips, cnty_fips, race_ethn, age) %>% 
+  group_by(year, state, st_fips, race_ethn, age) %>% 
   summarise(pop = sum(pop)) %>% 
   ungroup()
 
 
-#compare births to age 0 pop
-pop %>% 
-  filter(year==2018, age==0) %>% 
-  group_by(race_ethn) %>% 
-  summarise(pop = sum(pop))
-### looks fine
-
-saveRDS(births_grp, file = "births.RDS")
+# #compare births to age 0 pop
+# pop %>% 
+#   filter(year==2018, age==0) %>% 
+#   group_by(race_ethn) %>% 
+#   summarise(pop = sum(pop))
+# ### looks fine
+# 
+# saveRDS(births_grp, file = "births.RDS")
 
 saveRDS(pop, file = "pop.RDS")
